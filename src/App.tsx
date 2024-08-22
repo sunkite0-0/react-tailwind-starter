a57384e539245fd71bda9d84ff5b9daf3b982c3f
sunkite3-3 <83447421+sunkite3-3@users.noreply.github.com>
2024-08-22 21:02:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-900">Hello tailwind!</h1>
+    </>
+  );
+};
+export default App;
+
+// You can make basic code with "rafce".
